d1fb6c559aa460c1f3d5f19f27e1a444ab93e516
Edgar Vargas <vargased23@gmail.com>
2022-12-03 18:48:02 -0800

    started html

---
+++ script.js
@@ -0,0 +1,10 @@
+var start = document.getElementById("start")
+
+function timer()
+
+start.addEventListener("click", function()
+{ console.log("clicked")
+// once start is clicked timer starts counting down, increase counter for games played, genertate word
+// generato word, creat dashes for blank letters
+
+})
